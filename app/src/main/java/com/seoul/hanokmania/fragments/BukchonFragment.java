e8423b2b9a1db5342a93d56e4d908cadf1d0fc35
prChoe <massivcode@gmail.com>
2015-10-22 13:53:37 +0900

    북촌한옥마을 액션바에 실시간 검색기능 추가

--- app/src/main/java/com/seoul/hanokmania/fragments/BukchonFragment.java
+++ app/src/main/java/com/seoul/hanokmania/fragments/BukchonFragment.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -28,7 +29,7 @@ import java.util.List;
 /**
  * Created by Ray Choe on 2015-10-20.
  */
-public class BukchonFragment extends Fragment {
+public class BukchonFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     private static final String TAG = BukchonFragment.class.getSimpleName();
     private ExpandableListView mListView;
@@ -36,15 +37,19 @@ public class BukchonFragment extends Fragment {
     private List mGroupList;
     private List<List<BukchonItem>> mChildList;
 
+    private BukchonExpListAdapter mAdapter;
+    private SearchView mSearchView;
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_go_home:
                 Uri uri = Uri.parse("http://bukchon.seoul.go.kr/index.jsp");
-                Intent it  = new Intent(Intent.ACTION_VIEW,uri);
+                Intent it = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(it);
                 break;
-            case R.id.action_sort:
+            case R.id.action_search:
+
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -53,6 +58,9 @@ public class BukchonFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.bukchon, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        mSearchView = (SearchView) menuItem.getActionView();
+        mSearchView.setOnQueryTextListener(this);
     }
 
     public static BukchonFragment newInstance() {
@@ -68,6 +76,7 @@ public class BukchonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bukchon, container, false);
         mListView = (ExpandableListView) view.findViewById(R.id.expandable_list_view);
+
         setHasOptionsMenu(true);
         return view;
     }
@@ -77,6 +86,7 @@ public class BukchonFragment extends Fragment {
 
         mGroupList = new ArrayList();
         mChildList = new ArrayList<>();
+
 
         setData();
 
@@ -98,7 +108,9 @@ public class BukchonFragment extends Fragment {
                         HanokContract.HanokBukchonCol.BOOL_CULTURE,
                         HanokContract.HanokBukchonCol.HOUSE_CONTENT};
 
-        Cursor cursor = db.query(true, HanokContract.TABLES[7], projection, null, null, null, null, null, null);
+        String orderBy = HanokContract.HanokBukchonCol.HOUSE_NAME + " asc ";
+
+        Cursor cursor = db.query(true, HanokContract.TABLES[7], projection, null, null, null, null, orderBy, null);
 
         while (cursor.moveToNext()) {
             // GroupList data
@@ -114,7 +126,6 @@ public class BukchonFragment extends Fragment {
             String bool_culture = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.BOOL_CULTURE));
             String house_content = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_CONTENT));
 
-            Log.d(TAG, "설명 : " + house_content);
 
 
             if (house_name != null) {
@@ -127,9 +138,93 @@ public class BukchonFragment extends Fragment {
             }
         }
 
-        mListView.setAdapter(new BukchonExpListAdapter(getContext(), mGroupList, mChildList));
+
+        mAdapter = new BukchonExpListAdapter(getContext(), mGroupList, mChildList);
+
+        mListView.setAdapter(mAdapter);
 
     }
 
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        mSearchView.clearFocus();
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mGroupList = new ArrayList();
+        mChildList = new ArrayList<>();
+
+        SQLiteDatabase db = new HanokOpenHelper(getContext()).getReadableDatabase();
+
+        String[] projection = new String[]
+                {HanokContract.HanokBukchonCol._ID,
+                        HanokContract.HanokBukchonCol.TYPE_NAME,
+                        HanokContract.HanokBukchonCol.HOUSE_ID,
+                        HanokContract.HanokBukchonCol.HOUSE_NAME,
+                        HanokContract.HanokBukchonCol.HOUSE_ADDR,
+                        HanokContract.HanokBukchonCol.HOUSE_OWNER,
+                        HanokContract.HanokBukchonCol.HOUSE_TELL,
+                        HanokContract.HanokBukchonCol.HOUSE_HP,
+                        HanokContract.HanokBukchonCol.BOOL_CULTURE,
+                        HanokContract.HanokBukchonCol.HOUSE_CONTENT};
+
+        String selection = HanokContract.HanokBukchonCol.HOUSE_NAME + " LIKE ? OR " +
+                HanokContract.HanokBukchonCol.HOUSE_ADDR + " LIKE ? ";
+
+
+        String[] selectionArgs = {"%" + newText + "%", "%" + newText + "%"};
+
+        String orderBy = HanokContract.HanokBukchonCol.HOUSE_NAME + " asc ";
+
+        Cursor cursor = db.query(true, HanokContract.TABLES[7], projection, selection, selectionArgs, null, null, orderBy, null);
+
+        while (cursor.moveToNext()) {
+            // GroupList data
+            String house_name = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_NAME));
+
+            // ChildList data
+            String type_name = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.TYPE_NAME));
+            String house_id = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_ID));
+            String house_addr = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_ADDR));
+            String house_owner = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_OWNER));
+            String house_tell = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_TELL));
+            String house_hp = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_HP));
+            String bool_culture = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.BOOL_CULTURE));
+            String house_content = cursor.getString(cursor.getColumnIndexOrThrow(HanokContract.HanokBukchonCol.HOUSE_CONTENT));
+
+
+
+            if (house_name != null) {
+                mGroupList.add(house_name);
+                BukchonItem bukchonItem = new BukchonItem(house_name, house_addr, house_owner,
+                        house_tell, house_hp, bool_culture, type_name, house_content, house_id);
+                List<BukchonItem> childContent = new ArrayList<>();
+                childContent.add(bukchonItem);
+                mChildList.add(childContent);
+            }
+        }
+
+        mAdapter.swapData(mGroupList, mChildList);
+        return true;
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause");
+    mSearchView.clearFocus();
+        Log.d(TAG, "" + mSearchView.hasFocus());
+
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop");
+        mSearchView.clearFocus();
+        Log.d(TAG, "" + mSearchView.hasFocus());
+        super.onStop();
+    }
 }
